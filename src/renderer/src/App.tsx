dad6ac70d718fe78e3289b7718d93b3e8b7025aa
willgpennell <wpennell48@gmail.com>
2024-06-05 13:34:56 -0230

    mdx editor implemented and various content editable class names added aswell as plugins

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,6 +2,7 @@ import { RootLayout, Sidebar, Content } from '@/components'
 import { DraggableTopBar } from './components/DraggableTopBar'
 import { ActionButtonRow } from '@/components'
 import { NotePreviewList } from '@/components'
+import { MarkdownEditor } from './components/MarkdownEditor'
 
 export default function App() {
   return (
@@ -12,7 +13,9 @@ export default function App() {
           <ActionButtonRow className="flex mt-1 justify-between mb-1" />
           <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
-        <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
+        <Content className="border-l pt-4 bg-zinc-900/50 border-l-white/20">
+          <MarkdownEditor />
+        </Content>
       </RootLayout>
     </>
   )

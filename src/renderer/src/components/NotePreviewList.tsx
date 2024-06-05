e0e8acf9f8120a712993dfc6a9b3b398acc7e57b
willgpennell <wpennell48@gmail.com>
2024-06-05 11:10:12 -0230

    function to format notes

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -5,7 +5,9 @@ import { NotePreview } from './NotePreview'
 export function NotePreviewList({ ...props }: ComponentProps<'ul'>) {
   return (
     <ul {...props}>
+      {/** this is essentially a for loop for all elements within the notesMock array, except it applies each element to a <ul> */}
       {notesMock.map((note) => (
+        /** this takes in note.title as a key and all attributes from note as props (title and last edit time)*/
         <NotePreview key={note.title} {...note}></NotePreview>
       ))}
     </ul>

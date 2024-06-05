b214a3688a1d57221c0bd017d21948dd98acbb86
willgpennell <wpennell48@gmail.com>
2024-06-05 10:11:45 -0230

    sample notes displayable in sidebar

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,7 @@
 import { RootLayout, Sidebar, Content } from '@/components'
 import { DraggableTopBar } from './components/DraggableTopBar'
 import { ActionButtonRow } from '@/components'
+import { NotePreviewList } from '@/components'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <RootLayout>
         <Sidebar className="p-2">
           <ActionButtonRow className="flex mt-1 justify-between" />
+          <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
         <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
       </RootLayout>
---
+++ src/shared/models.ts
@@ -0,0 +1,12 @@
+/**
+ * Note information, like title, last edit time etc
+ */
+export type NoteInfo = {
+  title: string
+  lastEditTime: number
+}
+
+/**
+ * Represents the markdown content of the note itself
+ */
+export type NoteContent = string

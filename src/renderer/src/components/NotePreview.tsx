046ffa7ed5abfcad420f09cfe75feaf98743b9d5
willgpennell <wpennell48@gmail.com>
2024-06-05 10:53:30 -0230

    classes configured for note preview

--- src/renderer/src/components/NotePreview.tsx
+++ src/renderer/src/components/NotePreview.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@renderer/utils'
 import { NoteInfo } from '@shared/models'
 import { ComponentProps } from 'react'
 
@@ -17,5 +18,19 @@ export function NotePreview({
   className,
   ...props
 }: NotePreviewProps) {
-  return <div {...props}>NotePreviewProps</div>
+  return (
+    <div
+      className={cn(
+        'cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75',
+        {
+          'bg-zinc-400/75': isActive,
+          'hover:bg-zinc-500/75': !isActive
+        },
+        className
+      )}
+      {...props}
+    >
+      <h3>{title}</h3>
+    </div>
+  )
 }

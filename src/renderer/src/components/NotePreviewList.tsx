1ea59368a7ed5851325fb964319258f9e1e48b7f
willgpennell <wpennell48@gmail.com>
2024-06-05 11:44:51 -0230

    changed key in NotePreviewList

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -8,7 +8,7 @@ export function NotePreviewList({ ...props }: ComponentProps<'ul'>) {
       {/** this is essentially a for loop for all elements within the notesMock array, except it applies each element to a <ul> */}
       {notesMock.map((note) => (
         /** this takes in note.title as a key and all attributes from note as props (title and last edit time)*/
-        <NotePreview key={note.title} {...note}></NotePreview>
+        <NotePreview key={note.title + note.lastEditTime} {...note}></NotePreview>
       ))}
     </ul>
   )
